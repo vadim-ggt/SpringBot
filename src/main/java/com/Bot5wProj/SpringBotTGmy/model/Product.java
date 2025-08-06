d39822b69929fbd01887f63f2ff7bc631218c4fa
vadim-ggt <koval.cap.stop.j@gmail.com>
2025-08-07 00:44:49 +0300

    Внедрил бд, создал таблицы

--- src/main/java/com/Bot5wProj/SpringBotTGmy/model/Product.java
+++ src/main/java/com/Bot5wProj/SpringBotTGmy/model/Product.java
@@ -1,4 +1,33 @@
 package com.Bot5wProj.SpringBotTGmy.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
 public class Product {
+
+    @Id
+    private Long id;
+
+    private String url;
+
+    private String title;
+
+    private Double price;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
 }
--- src/main/java/com/Bot5wProj/SpringBotTGmy/model/User.java
+++ src/main/java/com/Bot5wProj/SpringBotTGmy/model/User.java
@@ -1,4 +1,30 @@
 package com.Bot5wProj.SpringBotTGmy.model;
 
+import com.fasterxml.jackson.annotation.JsonTypeId;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+
+
+
+
+import java.util.List;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+
+    @Id
+    private Long chatId;
+
+    private String username;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Product> trackedProducts;
 }
